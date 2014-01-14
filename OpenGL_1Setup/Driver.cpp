723fce12ded6a97ee8437568ac8f543cf60d652f
Cody Duncan <codyduncan1260@live.com>
2014-01-13 21:00:29 -0800

    Added Unofficial OpenGL Software Development Kit. Build instructions here: http://glsdk.sourceforge.net/docs/html/pg_build.html. Built using premake to generate projects and solutions, built in Visual Studio 2012.
    Added necessary include/lib directories and dependencies to OpenGL_1Setup.vcxproj

---
+++ OpenGL_1Setup/Driver.cpp
@@ -0,0 +1,43 @@
+#include <string>       //string
+#include <stdio.h>      //cout, endl
+#include <windows.h>    //windows header
+#include <fstream>      //file stream
+#include <limits>       //numeric limits
+#include <crtdbg.h>     //memory leak debugging
+
+//testing includes from all libraries
+
+//GL load
+#include <glload/gl_all.h>
+#include <glload/gl_3_3.h>
+#include <glload/gl_load.hpp>
+
+//GLFW
+#include <GL/glfw.h>
+
+//GL math
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+//GL util
+#include <glutil/glutil.h>
+
+//GL Mesh
+#include <glmesh/glmesh.h>
+
+//GL image
+#include <glimg/glimg.h>
+
+//freeglut
+#include <GL/freeglut.h>
+
+//boost
+#include <boost\algorithm\cxx11\all_of.hpp>
+
+
+
+int main()
+{
+    return 0;
+}
