2cc01f0d83eb406cb0fb88f888d2aea2e9cd80e2
Cody Duncan <codyduncan1260@live.com>
2014-01-17 22:45:33 -0800

    Creates OpenGL context with glut.

--- OpenGL_1Setup/Driver.cpp
+++ OpenGL_1Setup/Driver.cpp
@@ -5,39 +5,86 @@
 #include <limits>       //numeric limits
 #include <crtdbg.h>     //memory leak debugging
 
-//testing includes from all libraries
-
-//GL load
-#include <glload/gl_all.h>
-#include <glload/gl_3_3.h>
-#include <glload/gl_load.hpp>
-
-//GLFW
-#include <GL/glfw.h>
-
-//GL math
-#include <glm/glm.hpp>
-#include <glm/gtc/type_ptr.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-
-//GL util
-#include <glutil/glutil.h>
-
-//GL Mesh
-#include <glmesh/glmesh.h>
-
-//GL image
-#include <glimg/glimg.h>
-
-//freeglut
 #include <GL/freeglut.h>
+#include <glload\gl_load.hpp>
 
-//boost
-#include <boost\algorithm\cxx11\all_of.hpp>
+int ScreenWidth = 800;
+int ScreenHeight = 600;
 
-//AntTweakBar
-#include <AntTweakBar.h>
-int main()
+int WindowHandle;
+
+void Draw();
+void ResizeWindow(int newWidth, int newHeight);
+void KeyboardDown(unsigned char key, int x, int y);
+void KeyboardUp(unsigned char key, int x, int y);
+void MouseButton(int button, int state, int x, int y);
+void MouseMove(int x, int y);
+
+int main(int argc, char** argv)
 {
+    glutInit(&argc, argv);
+
+    glutInitContextVersion (3, 3);                      //opengl version
+    glutInitContextProfile(GLUT_COMPATIBILITY_PROFILE); //set for compatibility mode
+
+    glutInitWindowSize(ScreenWidth, ScreenHeight);
+
+    //sets the initial display mode
+    // double buffered
+    // RGBA mode window
+    // depth buffer mode
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+
+    //CREATE THE WINDOW
+    WindowHandle = glutCreateWindow("OpenGL setup Test");
+    if(WindowHandle < 1) 
+    {
+        fprintf(stderr,"ERROR: Could not create a new rendering window.\n");
+        exit(EXIT_FAILURE);
+    }
+    glutSetOption(GLUT_ACTION_ON_WINDOW_CLOSE, GLUT_ACTION_CONTINUE_EXECUTION);
+
+    //Load implementation of openGL functions automatically
+    //Alternatives to glLoad are documented here:http://www.opengl.org/wiki/OpenGL_Loading_Library
+    // GLEW, GL3W, OpenGL Loader Generator, GlLoad
+    glload::LoadFunctions();
+    
+    //hook function bindings
+    glutDisplayFunc(&Draw);
+    glutReshapeFunc(&ResizeWindow);
+
+    glutKeyboardFunc(&KeyboardDown);
+    glutKeyboardUpFunc(&KeyboardUp);
+
+    glutMouseFunc(&MouseButton);
+    glutMotionFunc(&MouseMove);
+
+    glutMainLoop();
+
     return 0;
+}
+
+void Draw()
+{
+    
+}
+void ResizeWindow(int newWidth, int newHeight)
+{
+    
+}
+void KeyboardDown(unsigned char key, int x, int y)
+{
+    
+}
+void KeyboardUp(unsigned char key, int x, int y)
+{
+    
+}
+void MouseButton(int button, int state, int x, int y)
+{
+    
+}
+void MouseMove(int x, int y)
+{
+    
 }
